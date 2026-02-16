a745d7745b265e50390e9719239f72ce47963f80
Louis van der Walt <louis@whirlylabs.com>
2026-02-16 13:11:31 +0200

    Revert "HACK around windows path issue"
    This reverts commit 2de8c6d330a31437982ef48999c0e884198085db.

--- joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/validation/DefaultImportsTests.scala
+++ joern-cli/frontends/kotlin2cpg/src/test/scala/io/joern/kotlin2cpg/validation/DefaultImportsTests.scala
@@ -4,14 +4,6 @@ import io.joern.kotlin2cpg.testfixtures.KotlinCode2CpgFixture
 import io.shiftleft.semanticcpg.language.*
 
 class DefaultImportsTests extends KotlinCode2CpgFixture(withOssDataflow = false) {
-  // TODO HACK: windows paths are not supported in SourceFiles.toRelativePath, so we need to filter out files with windows paths in SourceFilesPicker.shouldFilter.
-  private def normalizedTypeDeclFullNames(cpg: io.shiftleft.codepropertygraph.generated.Cpg): Set[String] = {
-    cpg.typ.typeDeclFullName.toSetImmutable.map {
-      case fullName if fullName.startsWith("<global>:C:/") => "<global>"
-      case fullName                                        => fullName
-    }
-  }
-
   // It tests if we take into consideration default imports: https://kotlinlang.org/docs/packages.html#default-imports
   "cpg.typ.typeDeclFullName of constructed CPG should remain the same regardless if one uses default imports or explicitly uses FQNs" should {
     "for kotlin.collections.mapOf" in {
@@ -27,8 +19,8 @@ class DefaultImportsTests extends KotlinCode2CpgFixture(withOssDataflow = false)
             |}
             |""".stripMargin)
 
-      val explicitRes       = normalizedTypeDeclFullNames(cpgExplicitFQNs)
-      val defaultImportsRes = normalizedTypeDeclFullNames(cpgDefaultImports)
+      val explicitRes       = cpgExplicitFQNs.typ.typeDeclFullName.toSetImmutable
+      val defaultImportsRes = cpgDefaultImports.typ.typeDeclFullName.toSetImmutable
       explicitRes shouldBe defaultImportsRes
     }
 
@@ -44,8 +36,8 @@ class DefaultImportsTests extends KotlinCode2CpgFixture(withOssDataflow = false)
             |}
             |""".stripMargin)
 
-      val explicitRes       = normalizedTypeDeclFullNames(cpgExplicitFQNs)
-      val defaultImportsRes = normalizedTypeDeclFullNames(cpgDefaultImports)
+      val explicitRes       = cpgExplicitFQNs.typ.typeDeclFullName.toSetImmutable
+      val defaultImportsRes = cpgDefaultImports.typ.typeDeclFullName.toSetImmutable
       explicitRes shouldBe defaultImportsRes
     }
   }
